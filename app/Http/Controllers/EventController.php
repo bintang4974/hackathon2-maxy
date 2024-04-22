8d3f0c8e6e94336b333a13532ed1b40f0f3192a3
endamahendra <endamahendra103@gmail.com>
2024-04-23 00:28:29 +0700

    Edit template

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $events = Event::all();
+        return view('events.index', compact('events'));
+    }
+
+    public function create()
+    {
+        return view('events.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'code' => 'required',
+            'title' => 'required',
+            'desc' => 'required',
+            'location' => 'required',
+            'price' => 'required|number',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $imageName = time() . '.' . $request->photo->extension();
+        $request->photo->move(public_path('images'), $imageName);
+
+        $event = Event::create([
+            'code' => $request->code,
+            'title' => $request->title,
+            'desc' => $request->desc,
+            'location' => $request->location,
+            'price' => $request->price,
+            'photo' => $imageName,
+        ]);
+
+        return redirect()->route('events.index')->with('success', 'Event berhasil dibuat.');
+    }
+
+    public function show(Event $event)
+    {
+        return view('events.show', compact('event'));
+    }
+
+    public function edit(Event $event)
+    {
+        return view('events.edit', compact('event'));
+    }
+
+    public function update(Request $request, Event $event)
+    {
+        $request->validate([
+            'code' => 'required',
+            'title' => 'required',
+            'desc' => 'required',
+            'location' => 'required',
+            'price' => 'required|number',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        $event->update($request->all());
+
+        if ($request->hasFile('photo')) {
+            $imageName = time() . '.' . $request->photo->extension();
+            $request->photo->move(public_path('images'), $imageName);
+            $event->photo = $imageName;
+            $event->save();
+        }
+
+        return redirect()->route('events.index')->with('success', 'Event berhasil diperbarui.');
+    }
+
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return redirect()->route('events.index')->with('success', 'Event berhasil dihapus.');
+    }
+}
